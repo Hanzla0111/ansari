806ceb515a3b202ba1c42f3a4ebaa057d7d7e36a
Hanzla Ansari <72706757+Hanzla0111@users.noreply.github.com>
2022-11-25 19:35:30 +0530

    jcodj

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java welcome to the punjab");  
+     System.out.println("Hello Java welcome to the punjab We are here for Doing Devops Course");  
     }  
 }  
