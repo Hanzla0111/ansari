a622104736412cc4856154a851c8f0f030573ba2
Hanzla Ansari <72706757+Hanzla0111@users.noreply.github.com>
2022-11-30 00:31:27 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java welcome to tdcdjiwdhwscsiudwbduhwdwhdiwhdidhwidhe punjab Wevnduicuicbsubsu are here for Doing Devopsalxma;lsjal;sn Courseuhdsidhikdgki");  
+     System.out.println("Hello Java ffgfghhwelcome to tdcdjiwdhwscsiudwbduhwdwhdiwhdidhwidhe punjab Wevnduicuicbsubsu are here for Doing Devopsalxma;lsjal;sn Courseuhdsidhikdgki");  
     }  
 }  
